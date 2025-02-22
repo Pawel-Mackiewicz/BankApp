1182c4f3b10667849c0859b65b0555d201f5e570
Pawel-Mackiewicz <pawel@mackiewicz.info>
2025-02-22 16:01:23 +0100

    feat: update versioning in pom.xml and enhance Dockerfile version update script

--- update-dockerfile-version.sh
+++ update-dockerfile-version.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-# Skrypt do aktualizacji wersji w Dockerfile
+# Nowa wersja przekazywana jako pierwszy argument
 VERSION=$1
 
-# Aktualizacja wersji w Dockerfile
-sed -i "s/^ARG VERSION=.*/ARG VERSION=${VERSION}/" Dockerfile
+# Aktualizacja wersji w linii COPY w Dockerfile, która zawiera nazwę jar
+# Wyszukujemy wzorzec: "COPY --from=builder /app/target/BankApp-" 
+# następnie sekwencję cyfr i kropek, potem ".jar app.jar"
+# i zastępujemy znalezioną wersję nową wersją.
+sed -i "s|\(COPY --from=builder /app/target/BankApp-\)[0-9.]\+\(\.jar app\.jar\)|\1${VERSION}\2|" Dockerfile
 
 echo "Dockerfile updated to version ${VERSION}"
