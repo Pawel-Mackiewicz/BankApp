feee98679bbe7fb4781bbfd60c0b0deda4d9b720
Pawel-Mackiewicz <pawel@mackiewicz.info>
2025-03-03 10:01:45 +0100

    Refactor/account model and lock manager for improved concurrency handling and code clarity

--- src/main/java/info/mackiewicz/bankapp/account/util/AccountLockManager.java
+++ src/main/java/info/mackiewicz/bankapp/account/util/AccountLockManager.java
@@ -1,19 +1,24 @@
 package info.mackiewicz.bankapp.account.util;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.ReentrantLock;
 
 import info.mackiewicz.bankapp.account.model.Account;
 import lombok.Getter;
 
 public class AccountLockManager {
     @Getter
-    public static AtomicInteger accountLockCounter;
+    public static AtomicInteger accountLockCounter = new AtomicInteger(0);
     @Getter
-    public static AtomicInteger accountUnlockCounter;
+    public static AtomicInteger accountUnlockCounter = new AtomicInteger(0);
 
-    static {
-        accountLockCounter = new AtomicInteger(0);
-        accountUnlockCounter = new AtomicInteger(0);
+    private static final Map<Integer, ReentrantLock> accountLocks = new ConcurrentHashMap<>();
+
+    // Metoda pomocnicza do pobierania/tworzenia locka
+    private static ReentrantLock getOrCreateLock(Integer accountId) {
+        return accountLocks.computeIfAbsent(accountId, k -> new ReentrantLock());
     }
 
     public static void lockAccounts(Account from, Account to) {
@@ -35,18 +40,34 @@ public class AccountLockManager {
             lockAccount(acc1);
         }
     }
-    private static void lockAccount(Account account) {
-        account.lock();
+
+    public static void lockAccount(Account account) {
+        getOrCreateLock(account.getId()).lock();
         accountLockCounter.incrementAndGet();
     }
 
     public static void unlockAccounts(Account from, Account to) {
-        if (to != null)     unlockAccount(to);
-        if (from != null)   unlockAccount(from);
+        if (from != null && to != null) {
+            unlockTwoAccounts(from, to);
+        } else if (from != null) {
+            unlockAccount(from);
+        } else if (to != null) {
+            unlockAccount(to);
+        }
     }
 
-    private static void unlockAccount(Account acc) {
-        acc.unlock();
+    private static void unlockTwoAccounts(Account acc1, Account acc2) {
+        if (acc1.getId() < acc2.getId()) {
+            unlockAccount(acc2);
+            unlockAccount(acc1);
+        } else {
+            unlockAccount(acc1);
+            unlockAccount(acc2);
+        }
+    }
+
+    private static void unlockAccount(Account account) {
+        getOrCreateLock(account.getId()).unlock();
         accountUnlockCounter.incrementAndGet();
     }
 }
