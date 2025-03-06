fb1c2069bdb50d6f1cfeb9e586cd51d7640fca23
Pawel-Mackiewicz <pawel@mackiewicz.info>
2025-03-06 14:08:09 +0100

    Refactor: remove unused imports in User model and AccountControllerTest

--- src/test/java/info/mackiewicz/bankapp/account/controller/AccountControllerTest.java
+++ src/test/java/info/mackiewicz/bankapp/account/controller/AccountControllerTest.java
@@ -13,7 +13,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import java.util.Arrays;
 import java.util.Collections;
 
-import org.iban4j.Iban;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
