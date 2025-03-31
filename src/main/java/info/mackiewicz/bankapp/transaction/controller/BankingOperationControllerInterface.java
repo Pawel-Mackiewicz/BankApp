9ab4f64507327b6635ae2838e3a91aab913f4653
Pawel-Mackiewicz <pawel@mackiewicz.info>
2025-03-31 15:34:20 +0200

    feat: Add BankingOperationControllerInterface and BankingOperationRequest for transaction operations

---
+++ src/main/java/info/mackiewicz/bankapp/transaction/controller/BankingOperationControllerInterface.java
@@ -0,0 +1,52 @@
+package info.mackiewicz.bankapp.transaction.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import info.mackiewicz.bankapp.transaction.model.dto.BankingOperationRequest;
+import info.mackiewicz.bankapp.transaction.model.dto.EmailTransferRequest;
+import info.mackiewicz.bankapp.transaction.model.dto.IbanTransferRequest;
+import jakarta.validation.Valid;
+
+/**
+ * Interface defining basic banking operations API endpoints.
+ * Provides contract for money transfers, withdrawals and deposits.
+ */
+public interface BankingOperationControllerInterface {
+
+    /**
+     * Transfers funds between accounts
+     * 
+     * @param request transfer details including source account IBAN, destination account IBAN and amount
+     * @param authUser authenticated user details, who has access to the source account
+     * @return response with transaction result
+     */
+    ResponseEntity<?> IbanTransfer(@Valid IbanTransferRequest request, UserDetails authUser);
+
+    /**
+     * Transfers funds to an email address
+     * 
+     * @param request transfer details including source account, destination email (which will be resolved to IBAN) and amount
+     * @param authUser authenticated user details, who has access to the source account
+     * @return response with transaction result
+     */
+    ResponseEntity<?> EmailTransfer(@Valid EmailTransferRequest request, UserDetails authUser);
+    
+    /**
+     * Withdraws funds from an account
+     * 
+     * @param request withdrawal details including account and amount
+     * @param authUser authenticated user details, who has access to the account
+     * @return response with transaction result
+     */
+    ResponseEntity<?> withdraw(@Valid BankingOperationRequest request, UserDetails authUser);
+    
+    /**
+     * Deposits funds to an account
+     * 
+     * @param request deposit details including account and amount
+     * @param authUser authenticated user details, who has access to the account
+     * @return response with transaction result
+     */
+    ResponseEntity<?> deposit(@Valid BankingOperationRequest request, UserDetails authUser);
+}
---
+++ src/main/java/info/mackiewicz/bankapp/transaction/model/dto/BankingOperationRequest.java
@@ -0,0 +1,36 @@
+package info.mackiewicz.bankapp.transaction.model.dto;
+
+import java.math.BigDecimal;
+
+import org.iban4j.Iban;
+
+import info.mackiewicz.bankapp.transaction.model.Transaction;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+
+/** 
+ * Class representing a banking operation request.
+ * This class serves as a base for various banking operations such as transfers.    
+ * It can also be used for withdrawals and deposits.
+ * It will be converted to Transaction.java objects
+ @see Transaction
+ */
+@Getter
+public class BankingOperationRequest {
+
+    @Schema(description = "IBAN of the active account", required = true)
+    @NotNull
+    private Iban activeAccountIban;
+    
+    @Schema(description = "Amount to be transferred", required = true)
+    @NotNull
+    private BigDecimal amount;
+
+    @Schema(description = "Title of the transaction", required = false)
+    @NotEmpty
+    private String title;
+
+}
