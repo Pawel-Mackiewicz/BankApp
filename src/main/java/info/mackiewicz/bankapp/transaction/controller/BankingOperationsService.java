006a238e5481a2b979ef6bc5cffc88248eb5c89a
Pawel-Mackiewicz <pawel@mackiewicz.info>
2025-04-01 11:38:05 +0200

    Refactors transaction handling with improved security
    Introduces dedicated services for better separation of concerns:
    - AccountSecurityService for ownership validation
    - TransactionBuilderService for transaction creation
    - Improves error handling and logging
    
    Moves transaction building logic to shared services for reusability
    Adds comprehensive input validation and security checks

--- src/main/java/info/mackiewicz/bankapp/transaction/controller/BankingOperationsService.java
+++ src/main/java/info/mackiewicz/bankapp/transaction/controller/BankingOperationsService.java
@@ -3,15 +3,19 @@ package info.mackiewicz.bankapp.transaction.controller;
 import org.iban4j.Iban;
 import org.springframework.stereotype.Service;
 
+import info.mackiewicz.bankapp.account.exception.AccountNotFoundByIbanException;
 import info.mackiewicz.bankapp.account.exception.AccountOwnershipException;
 import info.mackiewicz.bankapp.account.model.Account;
+import info.mackiewicz.bankapp.account.service.AccountSecurityService;
 import info.mackiewicz.bankapp.account.service.AccountService;
-import info.mackiewicz.bankapp.shared.service.IbanAnalysisService;
+import info.mackiewicz.bankapp.shared.exception.IbanAnalysisException;
+import info.mackiewicz.bankapp.shared.service.TransactionBuilderService;
+import info.mackiewicz.bankapp.transaction.exception.TransactionBuildingException;
+import info.mackiewicz.bankapp.transaction.exception.TransactionValidationException;
 import info.mackiewicz.bankapp.transaction.model.Transaction;
-import info.mackiewicz.bankapp.transaction.model.TransactionType;
 import info.mackiewicz.bankapp.transaction.model.dto.IbanTransferRequest;
 import info.mackiewicz.bankapp.transaction.model.dto.TransferResponse;
-import info.mackiewicz.bankapp.transaction.service.TransactionBuilderService;
+import info.mackiewicz.bankapp.transaction.service.TransactionService;
 import info.mackiewicz.bankapp.user.model.interfaces.UserDetailsWithId;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -22,47 +26,86 @@ import lombok.extern.slf4j.Slf4j;
 public class BankingOperationsService {
 
     private final AccountService accountService;
-    private final IbanAnalysisService ibanAnalysisService;
     private final TransactionBuilderService transactionBuilderService;
+    private final AccountSecurityService accountSecurityService;
+    private final TransactionService transactionService;
 
+    /**
+     * Handles the transfer of funds between accounts using IBANs.
+     *
+     * @param transferRequest The request containing transfer details
+     * @param user            The user initiating the transfer
+     * @return A response containing details of the transfer
+     * @throws AccountNotFoundByIbanException if no account is found with the given Iban
+     * @throws AccountOwnershipException      if the user does not own the source account
+     * @throws TransactionBuildingException   if the transaction cannot be built
+     * @throws TransactionValidationException if the transaction fails validation
+     * @throws IbanAnalysisException          if there is an error analyzing the IBANs
+     * 
+     */
     public TransferResponse handleIbanTransfer(IbanTransferRequest transferRequest, UserDetailsWithId user) {
+        log.info("Handling transfer from :{} to {}", transferRequest.getSourceIban(), transferRequest.getRecipientIban());
+
         Iban sourceIban = transferRequest.getSourceIban();
         Iban destinationIban = transferRequest.getRecipientIban();
 
-        Account sourceAccount = retrieveAccount(sourceIban);
-        validateAccountOwnership(user.getId(), sourceAccount);
+        Account validatedSourceAccount = retrieveValidatedAccount(user.getId(), sourceIban);
 
         Account destinationAccount = retrieveAccount(destinationIban);
 
-        TransactionType type = resolveTransferType(sourceIban, destinationIban);
+        Transaction transfer = createTransferTransaction(transferRequest, validatedSourceAccount, destinationAccount);
 
-        Transaction transfer = buildTransferTransaction(transferRequest, sourceAccount, destinationAccount, type);
-
-        return new TransferResponse(sourceAccount, destinationAccount, transfer);
+        log.info("Transfer transaction created: {}", transfer.getId());
+        return new TransferResponse(validatedSourceAccount, destinationAccount, transfer);
     }
 
+    /**
+     * Retrieves the account using the source Iban from the transfer request.
+     *
+     * @param accountIban The Iban of the account to retrieve
+     * @return The account associated with the given Iban
+     * @throws AccountNotFoundByIbanException if no account is found with the given Iban
+     */
     private Account retrieveAccount(Iban accountIban) {
         // Retrieve the account using the source Iban from the transfer request
         return accountService.getAccountByIban(accountIban);
     }
-    private void validateAccountOwnership(Integer userId, Account account) {
-        Integer ownerId = account.getOwner().getId();
 
-        // Check if the user is the owner of the account
-        if (!ownerId.equals(userId)) {
-            log.warn("Unauthorized access attempt by user ID: {}", userId);
-            throw new AccountOwnershipException("Unauthorized access to account");
-        }
+    /**
+     * Validates the ownership of the account using the user ID and Iban.
+     *
+     * @param userId      The ID of the user
+     * @param accountIban The Iban of the account to validate
+     * @return The validated account
+     * @throws AccountOwnershipException if the user does not own the account
+     * @throws AccountNotFoundByIbanException if no account is found with the given Iban
+     */
+    private Account retrieveValidatedAccount(Integer userId,  Iban accountIban) {
+        return accountSecurityService.validateAccountOwnership(userId, accountIban);
     }
 
-    private TransactionType resolveTransferType(Iban sourceIban, Iban destinationIban) {
-        return ibanAnalysisService.resolveTransferType(sourceIban, destinationIban);
-    }
+    /**
+     * Creates a transfer transaction using the transfer request and accounts.
+     * Transaction is registered in the system.
+     *
+     * @param transferRequest The request containing transfer details
+     * @param sourceAccount   The source account for the transfer
+     * @param destinationAccount The destination account for the transfer
+     * @return The created transaction
+     * @throws TransactionBuildingException if the transaction cannot be built
+     * @throws TransactionValidationException if the transaction fails validation
+     */
+    private Transaction createTransferTransaction(IbanTransferRequest transferRequest, Account sourceAccount,
+            Account destinationAccount) {
 
-    private Transaction buildTransferTransaction(IbanTransferRequest transferRequest, Account sourceAccount,
-            Account destinationAccount, TransactionType type) {
-        return transactionBuilderService.buildTransferTransaction(transferRequest, sourceAccount,
-                destinationAccount, type);
+        Transaction transaction =  transactionBuilderService.buildTransferTransaction(
+            transferRequest.getAmount(), 
+            transferRequest.getTitle(), 
+            sourceAccount,
+            destinationAccount
+            );
+        
+        return transactionService.registerTransaction(transaction);
     }
 
 
