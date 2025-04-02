431a554b4b53aa78f93ede19e79a0e8084263e78
Pawel-Mackiewicz <pawel@mackiewicz.info>
2025-04-02 18:04:19 +0200

    refactor: Change validation annotation from @NotEmpty to @NotNull for recipientIban in IbanTransferRequest

--- src/main/java/info/mackiewicz/bankapp/transaction/model/dto/IbanTransferRequest.java
+++ src/main/java/info/mackiewicz/bankapp/transaction/model/dto/IbanTransferRequest.java
@@ -1,11 +1,12 @@
 package info.mackiewicz.bankapp.transaction.model.dto;
 
+
 import org.iban4j.Iban;
 
 import info.mackiewicz.bankapp.account.validation.DifferentAccounts;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -16,6 +17,6 @@ import lombok.Setter;
 public class IbanTransferRequest extends BankingOperationRequest {
 
     @Schema(description = "The destination IBAN for the transfer", requiredMode = RequiredMode.REQUIRED) //is requireMode needed?
-    @NotEmpty
+    @NotNull
     private Iban recipientIban;
 }
