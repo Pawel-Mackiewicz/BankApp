ef778db470ab9273a5112df9cdeb82d138fcf5fa
Pawel-Mackiewicz <pawel@mackiewicz.info>
2025-04-11 19:17:32 +0200

    Add @Transactional to registerUser method in DefaultRegistrationService

--- src/main/java/info/mackiewicz/bankapp/system/registration/service/DefaultRegistrationService.java
+++ src/main/java/info/mackiewicz/bankapp/system/registration/service/DefaultRegistrationService.java
@@ -12,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 
@@ -28,8 +29,9 @@ public class DefaultRegistrationService implements RegistrationService {
     private final BonusGrantingService bonusGrantingService;
     private final EmailService emailService;
 
+    @Transactional
     public RegistrationResponse registerUser(RegistrationRequest request) {
-        MDC.put("Email Address", request.getEmail());
+        MDC.put("Email Address", request.getEmail().toString());
         try {
             log.info("Starting user registration process for email: {}", request.getEmail());
 
