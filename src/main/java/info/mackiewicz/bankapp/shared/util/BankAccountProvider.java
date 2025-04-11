8dc57eb81c341589433845e9da84037f84d84793
Pawel-Mackiewicz <pawel@mackiewicz.info>
2025-04-11 19:58:18 +0200

    Update default bank IBAN handling in BankAccountProvider
    Replaced the hardcoded default IBAN with a configurable value from `application.yaml`. Added support for Spring's `@Value` annotation to load the IBAN dynamically and initialized it during the `@PostConstruct` phase. This improves flexibility and allows environment-specific configurations.

--- src/main/java/info/mackiewicz/bankapp/shared/util/BankAccountProvider.java
+++ src/main/java/info/mackiewicz/bankapp/shared/util/BankAccountProvider.java
@@ -2,20 +2,34 @@ package info.mackiewicz.bankapp.shared.util;
 
 import info.mackiewicz.bankapp.account.model.Account;
 import info.mackiewicz.bankapp.account.service.AccountService;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.iban4j.Iban;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+/**
+ * Provides access to a default bank account associated with the application.
+ * This class acts as a utility to retrieve a specific bank account based on its predefined IBAN.
+ * The account retrieval is handled via the {@link AccountService}.
+ *
+ * The default IBAN is hardcoded as a constant and refers to a Polish IBAN (PL66485112340000000000000000).
+ */
 @Component
 @RequiredArgsConstructor
 public class BankAccountProvider {
 
-    private static final Iban DEFAULT_BANK_IBAN = Iban.valueOf("PL66485112340000000000000000");
+    @Value("${bankapp.bank.account.iban:PL66485112340000000000000000}")
+    private String defaultBankIbanString;
+    private Iban defaultBankIban;
+
+    @PostConstruct
+    private void init() {defaultBankIban = Iban.valueOf(defaultBankIbanString);}
 
     private final AccountService accountService;
 
     public Account getBankAccount() {
-        return accountService.getAccountByIban(DEFAULT_BANK_IBAN);
+        return accountService.getAccountByIban(defaultBankIban);
     }
 
 }
