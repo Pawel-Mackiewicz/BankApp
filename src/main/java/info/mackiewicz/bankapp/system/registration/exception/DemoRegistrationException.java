ec38bcdedf40dd85cd1bb2bd9c06779317d0a0be
Pawel-Mackiewicz <pawel@mackiewicz.info>
2025-04-14 23:19:43 +0200

    Add DemoRegistrationService and custom exception handling
    Introduced `DemoRegistrationService` to handle demo user registrations and encapsulate logic for interacting with the registration system. Added `DemoRegistrationException` to manage internal errors during the registration process, ensuring clear distinction between user-related issues and system issues.

---
+++ src/main/java/info/mackiewicz/bankapp/system/registration/exception/DemoRegistrationException.java
@@ -0,0 +1,16 @@
+package info.mackiewicz.bankapp.system.registration.exception;
+
+import info.mackiewicz.bankapp.shared.core.BankAppBaseException;
+import info.mackiewicz.bankapp.shared.core.error.ErrorCode;
+
+public class DemoRegistrationException extends BankAppBaseException {
+
+    private static final ErrorCode ERROR_CODE = ErrorCode.INTERNAL_ERROR;
+
+    public DemoRegistrationException(String message) {
+        super(message, ERROR_CODE);
+    }
+    public DemoRegistrationException(String message, Throwable cause) {
+        super(message, cause, ERROR_CODE);
+    }
+}
