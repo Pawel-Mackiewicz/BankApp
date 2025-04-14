498711336f16a1f965afccca47f900e0468884fb
Pawel-Mackiewicz <pawel@mackiewicz.info>
2025-04-14 15:14:39 +0200

    Add Javadoc to RegistrationService interface
    Added comprehensive Javadoc comments to the RegistrationService interface to describe its purpose, responsibilities, and key actions. This enhances code readability and provides clear documentation for developers integrating with the user registration logic.

--- src/main/java/info/mackiewicz/bankapp/system/registration/service/RegistrationService.java
+++ src/main/java/info/mackiewicz/bankapp/system/registration/service/RegistrationService.java
@@ -3,6 +3,21 @@ package info.mackiewicz.bankapp.system.registration.service;
 import info.mackiewicz.bankapp.system.registration.dto.RegistrationRequest;
 import info.mackiewicz.bankapp.system.registration.dto.RegistrationResponse;
 
+/**
+ * Defines a service responsible for handling user registration processes.
+ * This service encapsulates all the necessary logic to register a new user, 
+ * including user account creation, granting of a welcome bonus, and sending notifications.
+ *<p>
+ * The implementation of this interface ensures the following:
+ * <br>- Validating and processing the user registration request.
+ * <br>- Creating a new user entity based on the provided registration data.
+ * <br>- Generating a new user account and associating it with the registered user.
+ * <br>- Granting a predefined welcome bonus to the newly created account.
+ * <br>- Sending a welcome notification (e.g., email) to the registered user.
+ *<p>
+ * The method exposed in this service facilitates integration with external systems or components
+ * that require user registration capabilities.
+ */
 public interface RegistrationService {
 
     RegistrationResponse registerUser(RegistrationRequest request);
