ba4cf937335cb5be8d4829d892f0a53b2c93502d
Pawel-Mackiewicz <pawel@mackiewicz.info>
2025-04-15 16:16:17 +0200

    Refactor imports and add availableBalance property

--- src/main/java/info/mackiewicz/bankapp/account/model/Account.java
+++ src/main/java/info/mackiewicz/bankapp/account/model/Account.java
@@ -1,37 +1,28 @@
 package info.mackiewicz.bankapp.account.model;
 
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
-import java.util.Objects;
-
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
-import org.iban4j.Iban;
-
 import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import info.mackiewicz.bankapp.account.model.dto.AccountOwnerDTO;
 import info.mackiewicz.bankapp.account.model.interfaces.AccountInfo;
 import info.mackiewicz.bankapp.account.service.AccountService;
 import info.mackiewicz.bankapp.account.service.AccountServiceAccessManager;
 import info.mackiewicz.bankapp.account.util.IbanConverter;
 import info.mackiewicz.bankapp.user.model.User;
-import jakarta.persistence.Column;
-import jakarta.persistence.Convert;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import org.iban4j.Iban;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Objects;
 
 /**
  * Entity representing a bank account in the system.
  * Each account is associated with a single user and has a unique IBAN.
  * To create a new account, use {@link AccountService#createAccount}.
+ *
  * @see AccountService
  */
 @Entity
@@ -65,12 +56,15 @@ public class Account implements AccountInfo {
     @Getter
     private BigDecimal balance;
 
+    @Getter
+    private BigDecimal availableBalance;
+
     /**
      * Default constructor for JPA.
      * This constructor is package-private to prevent direct instantiation.
      * Use {@link AccountService} to create new accounts.
      */
-    Account() {
+    protected Account() {
     }
 
     /**
@@ -92,7 +86,7 @@ public class Account implements AccountInfo {
 
     /**
      * Creates a new AccountFactory instance.
-     * 
+     *
      * @return A new instance of AccountFactory
      */
     public static AccountFactory factory() {
@@ -102,10 +96,11 @@ public class Account implements AccountInfo {
     /**
      * Sets the balance of the account.
      * This method is protected by {@link AccountServiceAccessManager} and can only be called
-     * from {@link info.mackiewicz.bankapp.account.service.AccountOperationsService}.
+     * from AccountOperationsService.
      * Any unauthorized access will result in a {@link SecurityException} being thrown.
      *
      * @param newBalance The new balance to set
+     *
      * @throws SecurityException if called from an unauthorized context
      */
     public void setBalance(BigDecimal newBalance) {
@@ -131,11 +126,12 @@ public class Account implements AccountInfo {
     public AccountOwnerDTO getOwner() {
         return new AccountOwnerDTO(owner);
     }
-    
+
     @Override
     public String getOwnerFullname() {
         return owner.getFullName();
     }
+
     /**
      * Returns the actual User object that owns this account.
      * This method is for internal use only, for JSON serialization use getOwner()
@@ -164,6 +160,7 @@ public class Account implements AccountInfo {
      * Two accounts are considered equal if they have the same ID or the same IBAN.
      *
      * @param o The object to compare with
+     *
      * @return true if the objects are equal, false otherwise
      */
     @Override
