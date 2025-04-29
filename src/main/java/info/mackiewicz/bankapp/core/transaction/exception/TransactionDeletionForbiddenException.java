53e0c9d5083c9421fd57120e25ff36aaae6c7160
Pawel-Mackiewicz <pawel@mackiewicz.info>
2025-04-29 20:08:32 +0200

    Add validation and exception for restricted transaction deletion
    - Introduced `TransactionDeletionForbiddenException` for handling deletion attempts of non-deletable transactions.
    - Enhanced `ErrorCode` with `TRANSACTION_NOT_DELETABLE`.
    - Updated `TransactionCommandService` to implement deletion logic based on transaction status.
    - Extended unit tests to verify deletion rules for various transaction statuses (`NEW`, `DONE`, `PENDING`, etc.).

---
+++ src/main/java/info/mackiewicz/bankapp/core/transaction/exception/TransactionDeletionForbiddenException.java
@@ -0,0 +1,25 @@
+package info.mackiewicz.bankapp.core.transaction.exception;
+
+import info.mackiewicz.bankapp.system.error.handling.core.error.ErrorCode;
+
+/**
+ * Exception thrown when an attempt is made to delete a transaction that is not allowed to be deleted.
+ * <p>
+ * This exception is typically used in scenarios where specific business rules or transaction states
+ * forbid the deletion of certain transactions, such as transactions that are already processed
+ * or currently in process.
+ * <p>
+ * The associated error code for this exception is {@code ErrorCode.TRANSACTION_NOT_DELETABLE}.
+ */
+public class TransactionDeletionForbiddenException extends TransactionBaseException {
+
+    private static final ErrorCode ERROR_CODE = ErrorCode.TRANSACTION_NOT_DELETABLE;
+
+    public TransactionDeletionForbiddenException(String message) {
+        super(message, ERROR_CODE);
+    }
+
+    public TransactionDeletionForbiddenException(String message, Throwable cause) {
+        super(message, cause, ERROR_CODE);
+    }
+}
